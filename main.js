9b77f80c30c4754b852e82a4a468b4bdb0a6580d
Ondrej Kupka <ondra.cap@gmail.com>
2021-01-12 02:26:51 +0100

    wip
    [ci skip]

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const api = require('./gen/scheming/um/auth/v1/enforcer_api_grpc_web_pb');
 
-var client = new api.EnforcerAPIPromiseClient('http://localhost:8000');
+const client = new api.EnforcerAPIPromiseClient('http://localhost:8000');
 
 function enforce(enforcer_name, request) {
     const req = new api.EnforceRequest();
